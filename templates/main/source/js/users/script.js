85f9da9283ba5c57fe1669145e523cafb8e44720
kazanweb <kazanweb.studio@gmail.com>
2018-09-11 10:36:17 +0300

    fix

--- templates/main/source/js/users/script.js
+++ templates/main/source/js/users/script.js
@@ -1,45 +1,6 @@
-//Доступный hamburger https://foxland.fi/simple-accessible-svg-menu-hamburger-animation
-function hamburger(element, menu) {
-	var button = document.getElementById(element),
-		menu = document.getElementById(menu);
-	button.onclick = function () {
-		// Toggle class "opened". Set also aria-expanded to true or false.
-		if (-1 !== button.className.indexOf('opened')) {
-			button.className = button.className.replace(' opened', '');
-			button.setAttribute('aria-expanded', 'false');
-			menu.className = menu.className.replace(' active', '');
-			menu.setAttribute('aria-expanded', 'false');
-		} else {
-			button.className += ' opened';
-			button.setAttribute('aria-expanded', 'true');
-			menu.className += ' active';
-			menu.setAttribute('aria-expanded', 'true');
-		}
-	};
-}
-
 $(document).ready(function () {
 
 	window.globalPopup = new Popup();
-
-	// $(".js-combox").combox({
-	// 	startFn: function(li, index, combox) {
-
-	// 		this.input = combox.getElementsByTagName("input")[0];
-
-	// 		this.input.value = li.getAttribute("value");
-
-	// 	},
-	// 	changeFn: function(li, index, combox) {
-
-	// 		var _this = this;
-
-	// 		this.input.value = li.getAttribute("value");
-
-	// 	}
-	// });
-
-	hamburger('js-hamburger', "js-menu");
 
 	$("[type=tel]").inputmask("+7 (999) 999-99-99");
 
